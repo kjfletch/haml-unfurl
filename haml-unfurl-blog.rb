659c6dee7a11694a3e765d643a3cd67aca2dc537
Kevin John Fletcher <kevinjohn.fletcher@googlemail.com>
2010-12-01 18:39:19 +0000

    Handling template options is now done by the data class. Added 'class' option type.

--- haml-unfurl-blog.rb
+++ haml-unfurl-blog.rb
@@ -4,24 +4,82 @@ require 'fileutils'
 
 module HamlUnfurl
   class BlogUnfurlData
-    attr_accessor :doc, :title, :author, :datetime, :tags, :output_uri
+    attr_accessor :doc, :title, :author, :datetime, :class, :tags, :output_uri
 
-    def initialize()
-      @doc = nil
-      @title = nil
-      @author = nil
-      @datetime = nil
-      @tags = nil
-      @output_uri = nil
-    end
-  end
-
-  class BlogUnfurl
     @@uri_fmt_title = "{title}"
     @@uri_fmt_year = "{year}"
     @@uri_fmt_month = "{month}"
     @@uri_fmt_day = "{day}"
 
+    def initialize(doc)
+      @doc = doc
+
+      @title = @doc.get_lowest_option('title')
+      @title = 'No Title Set' if @title == nil
+
+      @author = @doc.get_lowest_option('author')
+      @author = 'No Author Set' if @author == nil
+
+      @class = @doc.get_lowest_option('class')
+      @class = '' if @class == nil
+
+      @datetime = @doc.get_lowest_option('time')
+      @datetime = getopt_datetime(@datetime) if @datetime != nil
+      @datetime = DateTime.new() if @datetime == nil
+
+      @tags = @doc.get_all_options('tags')
+      @tags = @tags.join(', ')
+      @tags = @tags.split(',')
+      @tags = @tags.map {|x| x.strip() }
+      @tags.uniq!()
+      @tags.delete('')
+
+      ## We do this after all other data vars are set up as they may
+      ## be referenced by the uri expand for the filename.
+      @output_uri = @doc.get_lowest_option('output')
+      @output_uri = "#{@@uri_fmt_title}.html" if @output_uri == nil
+      @output_uri = get_output_file(@output_uri)
+    end
+
+    def get_output_file(uri_fmt)
+      uri = uri_fmt
+
+      if uri.include?(@@uri_fmt_title)
+        if not @title
+          throw "No document title defined but output URI format specifies a title replacement."
+        end
+
+        uri.gsub!(@@uri_fmt_title, HamlUnfurl::uri_safe(@title))
+      end
+
+      if uri.include?(@@uri_fmt_year) or uri.include?(@@uri_fmt_month) or uri?(@@uri_fmt_day)
+        if not @datetime
+          throw "No document datetime defined but output URI format specifies a datetime replacement."
+        end
+
+        uri.gsub!(@@uri_fmt_year, "#{@datetime.year}")
+        uri.gsub!(@@uri_fmt_month, "%02d" % [@datetime.month])
+        uri.gsub!(@@uri_fmt_day, "%02d" % [@datetime.day])
+      end
+      
+      return uri
+    end
+
+    def getopt_datetime(datetime)
+      time_key = 'time'
+
+      begin
+        return Date::strptime(datetime, "%Y-%m-%d %H:%M")
+      rescue ArgumentError
+        
+      end
+
+      return nil
+    end
+
+  end
+
+  class BlogUnfurl
     def initialize ()
       @dirs = []
       @include_dirs = []
@@ -33,32 +91,8 @@ module HamlUnfurl
       file_list = get_file_list()
 
       file_list.each do |file|
-        doc_data = BlogUnfurlData.new()
-        doc_data.doc = Unfurl.new(file, @include_dirs)
-        
-        doc_data.title = doc_data.doc.get_lowest_option('title')
-        doc_data.title = 'No Title Set' if doc_data.title == nil
-
-        doc_data.author = doc_data.doc.get_lowest_option('author')
-        doc_data.author = 'No Author Set' if doc_data.author == nil
-
-        doc_data.datetime = doc_data.doc.get_lowest_option('time')
-        doc_data.datetime = getopt_datetime(doc_data.datetime) if doc_data.datetime != nil
-        doc_data.datetime = DateTime.new() if doc_data.datetime == nil
-
-        doc_data.tags = doc_data.doc.get_all_options('tags')
-        doc_data.tags = doc_data.tags.join(', ')
-        doc_data.tags = doc_data.tags.split(',')
-        doc_data.tags = doc_data.tags.map {|x| x.strip() }
-        doc_data.tags.uniq!()
-        doc_data.tags.delete('')
-
-        ## We do this after all other data vars are set up as they may
-        ## be referenced by the uri expand for the filename.
-        doc_data.output_uri = doc_data.doc.get_lowest_option('output')
-        doc_data.output_uri = "#{@@uri_fmt_title}.html" if doc_data.output_uri == nil
-        doc_data.output_uri = get_output_file(doc_data.output_uri, doc_data)
-        
+        doc = Unfurl.new(file, @include_dirs)
+        doc_data = BlogUnfurlData.new(doc)
         docs << doc_data
       end
 
@@ -96,49 +130,6 @@ module HamlUnfurl
       end
     end
 
-    def getopt_datetime(datetime)
-      time_key = 'time'
-
-      begin
-        return Date::strptime(datetime, "%Y-%m-%d %H:%M")
-      rescue ArgumentError
-        
-      end
-
-      return nil
-    end
-
-    def get_output_file(uri_fmt, doc)
-      uri = uri_fmt
-
-      if uri.include?(@@uri_fmt_title)
-        if not doc.title
-          throw "No document title defined but output URI format specifies a title replacement."
-        end
-
-        uri.gsub!(@@uri_fmt_title, uri_safe(doc.title))
-      end
-
-      if uri.include?(@@uri_fmt_year) or uri.include?(@@uri_fmt_month) or uri?(@@uri_fmt_day)
-        if not doc.datetime
-          throw "No document datetime defined but output URI format specifies a datetime replacement."
-        end
-
-        uri.gsub!(@@uri_fmt_year, "#{doc.datetime.year}")
-        uri.gsub!(@@uri_fmt_month, "%02d" % [doc.datetime.month])
-        uri.gsub!(@@uri_fmt_day, "%02d" % [doc.datetime.day])
-      end
-      
-      return uri
-    end
-
-    def uri_safe(nonsafe)
-      nonsafe = nonsafe.gsub(/[^a-zA-Z0-9_]/, '_')
-      nonsafe = nonsafe.gsub(/_{2,}/, '_')
-      nonsafe = nonsafe.gsub(/^_|_$/, '')
-      return nonsafe
-    end
-
     def get_file_list()
       file_list = []
       
@@ -154,6 +145,13 @@ module HamlUnfurl
 
       return file_list
     end
+  end
+
+  def self.uri_safe(nonsafe)
+    nonsafe = nonsafe.gsub(/[^a-zA-Z0-9_]/, '_')
+    nonsafe = nonsafe.gsub(/_{2,}/, '_')
+    nonsafe = nonsafe.gsub(/^_|_$/, '')
+    return nonsafe
   end
 
   blog_unfurl = BlogUnfurl.new()
